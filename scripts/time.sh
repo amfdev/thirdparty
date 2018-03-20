eddc0e57b4a7d92c54791a40a6d10f3894e7f65f
Alexander Kravchenko <mr.aleksandr.kravchenko@gmail.com>
2018-03-20 09:56:10 +0300

    update

--- scripts/time.sh
+++ scripts/time.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 set -x
+COMMAND=($@)
 
 TIME_DIR=$( dirname "${BASH_SOURCE[0]}")
 [ -z "$LOG_FILE" ] && LOG_FILE=$TIME_DIR/log.txt
 
 SECONDS=0
-echo ---- starting : "$@" >> $LOG_FILE
+echo "exec : ${COMMAND[0]}" >> $LOG_FILE
 "$@"
 
-echo ---- end: "$@" >> $LOG_FILE
-echo ---- time: $SECONDS seconds >> $LOG_FILE
+echo "time: ${SECONDS}s. (${COMMAND[0]})" >> $LOG_FILE
