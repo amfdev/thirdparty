d3fe7ea6b79306c9e48145fee8d931acd879be95
Alexander Kravchenko <mr.aleksandr.kravchenko@gmail.com>
2018-03-20 11:51:24 +0300

    update

--- install/build_env/install_all.sh
+++ install/build_env/install_all.sh
@@ -2,13 +2,17 @@
 
 set -x
 
+ROOT_DIR=$PWD
+
+. "$ROOT_DIR/../../scripts/toolset/common" || exit 1
+
+
 sudo install_deps.sh
 ./install_nasm.sh
 
-ROOT_DIR=$PWD
 
 if [ -z "$PREFIX" ]; then
-    PREFIX=${ROOT_DIR}/../../libs/mingw-w64
+    PREFIX=${ROOT_DIR}/mingw-w64
     rm -fR $PREFIX
     mkdir -p $PREFIX
     PREFIX=`readlink -f ${PREFIX}`
