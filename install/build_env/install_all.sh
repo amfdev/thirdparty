fe2a5107cbf52f177362edda19f874e9a5f452da
Alexander Kravchenko <mr.aleksandr.kravchenko@gmail.com>
2018-03-24 00:20:11 +0300

    update

--- install/build_env/install_all.sh
+++ install/build_env/install_all.sh
@@ -14,9 +14,9 @@ chmod +x $ROOT_DIR/../../scripts/time.sh
 chmod +x ./*.sh
 
 time.sh ./install_deps.sh
-time.sh ./install_nasm.sh
-
 time.sh ./install_mingw.sh
+
+time.sh ./install_nasm.sh
 time.sh ./install_mingw-gdb.sh
 time.sh ./install_sdl2.sh
 time.sh ./install_x264.sh
