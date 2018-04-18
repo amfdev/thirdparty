c46f8c47a1480b6097809ca9a277e170554a9132
Alexander Kravchenko <akravchenko188@gmail.com>
2018-04-19 00:00:54 +0300

    reorg build scripts

--- install/build_env/install_deps.sh
+++ install/build_env/install_deps.sh
@@ -14,9 +14,31 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
     sudo apt install -y cmake
     sudo apt install -y mc
 
+
+#from handsbrake
     sudo apt install -y autoconf
     sudo apt install -y python
     sudo apt install -y zlib1g-dev
     sudo apt install -y libtool-bin
+    sudo apt install -y yasm
+
+    sudo apt install -y autoconf
+    sudo apt install -y automake
+    sudo apt install -y build-essential
+    sudo apt install -y cmake
+    sudo apt install -y curl
+    sudo apt install -y gcc
+    sudo apt install -y git
+    sudo apt install -y libtool
+    sudo apt install -y libtool-bin
+    sudo apt install -y m4
+    sudo apt install -y make
+    sudo apt install -y patch
+    sudo apt install -y pkg-config
+    sudo apt install -y python
+    sudo apt install -y tar
+    sudo apt install -y yasm
+    sudo apt install -y zlib1g-dev
+    sudo apt install -y ragel
 fi
 
