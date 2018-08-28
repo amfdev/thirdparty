0ef3e19a4a7c732d0f6d1efb81b07e8150ab44ea
Alexander Kravchenko <akravchenko188@gmail.com>
2018-08-28 23:50:47 +0300

    deps

--- install/build_env/install_deps.sh
+++ install/build_env/install_deps.sh
@@ -6,39 +6,23 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
 
     sudo -- sh -c 'apt-get update; apt-get upgrade -y; apt-get dist-upgrade -y; apt-get autoremove -y; apt-get autoclean -y'
 
+    sudo apt update
+    sudo apt install -y autoconf automake build-essential \
+                cmake git libass-dev libbz2-dev libfontconfig1-dev libfreetype6-dev libfribidi-dev 
+                libharfbuzz-dev libjansson-dev libmp3lame-dev libogg-dev libopus-dev libsamplerate-dev \
+                libtheora-dev libtool libvorbis-dev libx264-dev \
+                libxml2-dev m4 make patch pkg-config python tar yasm zlib1g-dev
+
+    sudo apt install -y intltool libappindicator-dev libdbus-glib-1-dev libglib2.0-dev \
+                libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev \
+                libgtk-3-dev libgudev-1.0-dev libnotify-dev libwebkitgtk-3.0-dev
+
+
     sudo apt install -y wget
-    sudo apt install -y flex texinfo bison
-    sudo apt install -y build-essential
-    sudo apt install -y libmpc-dev
-    sudo apt install -y pkg-config
-    sudo apt install -y cmake
+    sudo apt install -y flex texinfo bison pax
+    sudo apt install -y libtool-bin
+
     sudo apt install -y mc
 
-
-#from handsbrake
-    sudo apt install -y autoconf
-    sudo apt install -y python
-    sudo apt install -y zlib1g-dev
-    sudo apt install -y libtool-bin
-    sudo apt install -y yasm
-
-    sudo apt install -y autoconf
-    sudo apt install -y automake
-    sudo apt install -y build-essential
-    sudo apt install -y cmake
-    sudo apt install -y curl
-    sudo apt install -y gcc
-    sudo apt install -y git
-    sudo apt install -y libtool
-    sudo apt install -y libtool-bin
-    sudo apt install -y m4
-    sudo apt install -y make
-    sudo apt install -y patch
-    sudo apt install -y pkg-config
-    sudo apt install -y python
-    sudo apt install -y tar
-    sudo apt install -y yasm
-    sudo apt install -y zlib1g-dev
-    sudo apt install -y ragel
 fi
 
